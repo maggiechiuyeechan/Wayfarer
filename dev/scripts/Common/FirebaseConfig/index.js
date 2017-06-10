24e66383ef602443aec2ad7cc14062eaa8d696d9
Maggie Chan <mach.sq@gmail.com>
2017-06-10 12:46:54 -0400

    upload images

---
+++ dev/scripts/Common/FirebaseConfig/index.js
@@ -0,0 +1,19 @@
+const config = {
+	apiKey: "AIzaSyAmLE5kYMWiB6-Hy-CAO_kBfRIEwJaNDkE",
+	authDomain: "wayfarer-17532.firebaseapp.com",
+	databaseURL: "https://wayfarer-17532.firebaseio.com",
+	projectId: "wayfarer-17532",
+	storageBucket: "wayfarer-17532.appspot.com",
+	messagingSenderId: "914349967715"
+};
+
+firebase.initializeApp(config);
+const databaseRef = firebase.database().ref();
+const storageRef = firebase.storage().ref();
+
+export default { databaseRef };
+export { storageRef };
+// export default firebase = { 
+// 	databaseRef: firebase.database().ref(),
+// 	storageRef: firebase.storage().ref()
+// }
--- dev/scripts/components/inputForm/index.js
+++ dev/scripts/components/inputForm/index.js
@@ -1,20 +1,37 @@
 import React from "react";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
-
+import Firebase, { storageRef } from "../../Common/FirebaseConfig";
 
 export default class Map extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			currentLocation: {}
+		}
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleSubmit(e) {
+		e.preventDefault();
+		console.log(this.file.files)
+
+		// let file = this.file.files[0];
+		// let thisImage = storageRef.child(this.file.files.name);
+		// thisImage.put(file).then((snapshot)=>{console.log("uploaded");});
+		// Firebase.databaseRef.push(this.state.currentLocation.coordinates);
+	}
+
 	componentWillReceiveProps(nextProps) {
-		console.log(nextProps);
+
 		if (nextProps.map !== null) {
 
-			let geocoder = new MapboxGeocoder({
+			mapboxgl.geocoder = new MapboxGeocoder({
 			    accessToken: mapboxgl.accessToken
 			});
-			nextProps.map.addControl(geocoder);
 
-			geocoder.on('result', function(ev) {
-				//console.log(ev.result.geometry)
-			    //map.getSource('single-point').setData(ev.result.geometry);
+			nextProps.map.addControl(mapboxgl.geocoder);
+
+			mapboxgl.geocoder.on('result', (event) => {
+				this.setState({ currentLocation:event.result.geometry });
 			});
 
 		}
@@ -22,6 +39,10 @@ export default class Map extends React.Component {
 	render(){
 		return(
 			<div>
+				<form onSubmit={this.handleSubmit}> 
+					<input type ="file" ref= {(ref)=> this.file = ref} multiple/>
+					<input type="submit" value="Submit" />
+				</form>
 			</div>
 		)
 	}
